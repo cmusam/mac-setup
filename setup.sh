ac97e700b5035c2bff3c20c7372d38d925f92a1e
Yan Shen <yanshen@Yans-MacBook.local>
2019-01-16 01:15:04 -0500

    init

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# install Homebrew at https://brew.sh
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install java
+brew install maven
+brew install tree
+brew install python
+
+brew cask install 1password
+brew cask install dropbox
+brew cask install wechat
+brew cask install sublime-text
+brew cask install slack
+brew cask install google-chrome
+
