494a754102cd8cd908204ee49712f8b6709d5375
Yan Shen <yanshen@Yans-MacBook.wv.cc.cmu.edu>
2019-01-21 16:46:00 -0500

    update

--- setup.sh
+++ setup.sh
@@ -7,12 +7,16 @@ brew install wget
 brew install youtube-dl
 brew install tree
 brew install python
-brew install awscli
 
 brew cask install 1password
 brew cask install dropbox
-brew cask install wechat
-brew cask install sublime-text
-brew cask install slack
 brew cask install google-chrome
+brew cask install skype
+brew cask install slack
+brew cask install sublime-text
+brew cask install wechat
+
+# cloud
+brew install awscli
+brew install terraform
 
