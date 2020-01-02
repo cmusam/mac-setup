41641c3d7c4accf432c9d8f5232f96831834674d
YS <samshens@yahoo.com>
2020-01-02 13:27:38 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,28 @@
 # install Homebrew at https://brew.sh
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# install java first
 brew cask install java
+
+# brew install: command line software
+brew install htop
 brew install maven
+brew install node
+brew install python
+brew install tldr
+brew install tree
 brew install wget
 brew install youtube-dl
-brew install tree
-brew install python
 
+# brew cask install: graphical applications
+# https://formulae.brew.sh/cask/
 brew cask install 1password
+brew cask install adobe-acrobat-reader
+brew cask install clion
 brew cask install dropbox
+brew cask install expressvpn  # may fail
 brew cask install google-chrome
+brew cask install omnidisksweeper
 brew cask install skype
 brew cask install slack
 brew cask install sublime-text
